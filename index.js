cfa99ab3c895e930642b2a275f89195f76dc3baa
Maneesha Garikipati <maneeshag099@gmail.com>
2025-08-15 18:54:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css' // Importing the Book component
+import reportWebVitals from './reportWebVitals';
+import Array from "./Array"; // Importing the Array component
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  
+   
+   <Array />  
+  </React.StrictMode>
+
+
+);
+reportWebVitals();
